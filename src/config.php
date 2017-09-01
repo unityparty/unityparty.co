f63da833cadd04f1ec2a07090962eac8361ff50a
Breadinator <br3adina7or@gmail.com>
2017-09-01 13:49:17 +0100

    General updates

--- src/config.php
+++ src/config.php
@@ -2,11 +2,12 @@
 
 	// DATABASE DETAILS (to set up, see details on the github) 
 	$config['dbaddr'] = 'localhost';
-	$config['dbname'] = 'database-name';
+	$config['dbname'] = 'table';
 	$config['dbuser'] = 'username';
 	$config['dbpass'] = "password";
 
 	// MISC
 	$config['notice'] = "Under construction";
+	//$config['notice'] = null; // <-- uncomment and comment the above to remove the notice completely
 
 ?>
